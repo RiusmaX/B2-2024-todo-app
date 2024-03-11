1f3b6176da22d64dd8faa13464b399705b76ec01
Marius <marius.sergent@gmail.com>
2024-03-11 14:51:57 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@nextui-org/react'
+import './App.css'
+// import Clock from './components/Clock'
+
+function App () {
+  return (
+    <>
+      <Button
+        color='primary'
+      >
+        Mon Bouton
+      </Button>
+    </>
+  )
+}
+
+export default App
