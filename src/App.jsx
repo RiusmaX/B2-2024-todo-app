25295f12b06a0d025b76fbb270e9f4e1c53ef17b
Marius <marius.sergent@gmail.com>
2024-03-12 11:49:03 +0100

    add todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { Button } from '@nextui-org/react'
 import './App.css'
+import TodoList from './components/TodoList'
 // import Clock from './components/Clock'
+
+const todos = [
+  {
+    title: 'TODO 1',
+    description: 'TODO DESCRIPTION 1'
+  },
+  {
+    title: 'TODO 2',
+    description: 'TODO DESCRIPTION 2'
+  },
+  {
+    title: 'TODO 3',
+    description: 'TODO DESCRIPTION 3'
+  },
+  {
+    title: 'TODO 4',
+    description: 'TODO DESCRIPTION 4'
+  }
+]
 
 function App () {
   return (
     <>
+      <TodoList
+        todos={todos}
+      />
       <Button
         color='primary'
       >
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,21 @@
+import Todo from './Todo'
+
+function TodoList ({ todos }) {
+  return (
+    <div className='flex flex-col gap-4 py-2 px-4'>
+      {
+        todos.map((todo) => {
+          return (
+            <Todo
+              key={todo.title}
+              title={todo.title}
+              description={todo.description}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default TodoList
